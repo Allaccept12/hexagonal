5ef35336385313d56d1f071517a52b719328a485
jay <ekdmd9201@gmail.com>
2024-04-24 21:03:22 +0900

    add user adapter

---
+++ src/main/kotlin/com/example/hexagonal/user/adapter/out/persistence/ExistsUserPortAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.hexagonal.user.adapter.out.persistence
+
+import com.example.hexagonal.user.application.port.out.persistence.ExistsUserPort
+import com.example.hexagonal.user.data.jpa.repository.UserEntityRepository
+import org.springframework.stereotype.Component
+
+@Component
+class ExistsUserPortAdapter(
+    private val userEntityRepository: UserEntityRepository
+): ExistsUserPort {
+
+    override fun invokeByUsername(username: String): Boolean {
+        return userEntityRepository.existsByUsername(username)
+    }
+
+
+}
--- src/main/kotlin/com/example/hexagonal/user/application/port/out/persistence/ExistsUserPort.kt
+++ src/main/kotlin/com/example/hexagonal/user/application/port/out/persistence/ExistsUserPort.kt
@@ -1,4 +1,6 @@
 package com.example.hexagonal.user.application.port.out.persistence
 
 interface ExistsUserPort {
+
+    fun invokeByUsername(username: String): Boolean
 }
